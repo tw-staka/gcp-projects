3d8a8645d49161e031077a31d2f59b1ca17d0bce
Paul Valla <pvalla@thoughtworks.com>
2019-03-27 12:11:47 +1100

    Allow terraform service account to act as the default compute engine service account

--- application-project.tf
+++ application-project.tf
@@ -58,3 +58,11 @@ resource "google_project_iam_member" "container_admin" {
   role       = "roles/container.admin"
   member     = "serviceAccount:${module.application_project.terraform_email}"
 }
+
+data "google_compute_default_service_account" "default" { }
+# Allow terraform service account to act as default compute service account
+resource "google_service_account_iam_member" "gce-default-account-iam" {
+  service_account_id = "${data.google_compute_default_service_account.default.name}"
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${module.application_project.terraform_email}"
+}
