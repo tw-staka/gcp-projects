3e2cf36acaca0c25ae300a1cd25bf55e9f1cce00
Paul Valla <pvalla@thoughtworks.com>
2019-03-29 16:06:25 +1100

    Enable kms api to decode secrets in CI for app project

--- application-project.tf
+++ application-project.tf
@@ -22,28 +22,31 @@ resource "google_project_service" "gke" {
 resource "google_project_service" "container_registry" {
   project = "${module.application_project.project_id}"
   service = "containerregistry.googleapis.com"
-  # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
   disable_dependent_services = true
   disable_on_destroy = false
 }
 resource "google_project_service" "container_analysis" {
   project = "${module.application_project.project_id}"
   service = "containeranalysis.googleapis.com"
-  # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
   disable_dependent_services = true
   disable_on_destroy = false
 }
 resource "google_project_service" "cloudbuild" {
   project = "${module.application_project.project_id}"
   service = "cloudbuild.googleapis.com"
-  # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
   disable_dependent_services = true
   disable_on_destroy = false
 }
 resource "google_project_service" "cloud_source_repository" {
   project = "${module.application_project.project_id}"
   service = "sourcerepo.googleapis.com"
-  # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed.
+  disable_dependent_services = true
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "kms" {
+  project = "${module.application_project.project_id}"
+  service = "cloudkms.googleapis.com"
   disable_dependent_services = true
   disable_on_destroy = false
 }
