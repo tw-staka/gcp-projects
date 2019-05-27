6ff948ff6afda9f9abe4321cc560919ddafe2b55
Sreeja Gupta <sreejagpt@gmail.com>
2019-05-27 11:09:55 +1000

    Adding an init script to create projects, service account and add permissions for the first time

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash -e
+
+export TF_CREDS=~/.config/gcloud/${PROJECT_ID}.json
+export TF_VAR_region="australia-southeast1"
+
+#Create Project
+gcloud projects create ${PROJECT_ID} --organization ${TF_VAR_org_id} --set-as-default
+
+#Set Default Project
+gcloud config set project ${PROJECT_ID}
+
+#Link Billing Account
+gcloud beta billing projects link ${PROJECT_ID} --billing-account ${TF_VAR_billing_account}
+
+#Create Service Accounts
+gcloud iam service-accounts create terraform \
+  --display-name "Terraform admin account"
+
+gcloud iam service-accounts keys create ${TF_CREDS} \
+  --iam-account terraform@${PROJECT_ID}.iam.gserviceaccount.com
+
+export GOOGLE_APPLICATION_CREDENTIALS=${TF_CREDS}
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com \
+  --role roles/viewer
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com \
+  --role roles/storage.admin
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:terraform@${$PROJECT_ID}.iam.gserviceaccount.com \
+  --role roles/container.admin
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:terraform@${PROJECT_ID}.iam.gserviceaccount.com \
+  --role roles/iam.serviceAccountUser
+
+
+#Print Out Default Creds for CircleCI
+echo "Created DEFAULT_APPLICATION_CREDENTIALS_FILE for CircleCI! \
+
+DEFAULT_APPLICATION_CREDENTIALS_FILE= `cat ${TF_CREDS} | base64`"
